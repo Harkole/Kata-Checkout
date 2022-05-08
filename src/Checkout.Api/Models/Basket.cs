ee0f3dd03d2386355a2c4de556f0fc8638e1f70d
Will Blackburn <harkole@gmail.com>
2022-05-08 19:14:48 +0100

    Added first draft of models

---
+++ src/Checkout.Api/Models/Basket.cs
@@ -0,0 +1,23 @@
+﻿namespace Checkout.Api.Models
+{
+    /// <summary>
+    /// Details about a basket
+    /// </summary>
+    public class Basket
+    {
+        /// <summary>
+        /// The client the basket belongs too
+        /// </summary>
+        public int ClientIdentity { get; set; }
+
+        /// <summary>
+        /// The items in the basket
+        /// </summary>
+        public IDictionary<string, int> Items { get; set; } = new Dictionary<string, int>();
+
+        /// <summary>
+        /// The total cost of the basket
+        /// </summary>
+        public int TotalCost { get; set; }
+    }
+}
---
+++ src/Checkout.Api/Models/User.cs
@@ -0,0 +1,23 @@
+﻿namespace Checkout.Api.Models
+{
+    /// <summary>
+    /// Details about a Client of the site
+    /// </summary>
+    public class Client
+    {
+        /// <summary>
+        /// Primary email address
+        /// </summary>
+        public string Email { get; set; } = default!;
+
+        /// <summary>
+        /// What the client would like to be known as
+        /// </summary>
+        public string Name { get; set; } = default!;
+
+        /// <summary>
+        /// The internal system identity of the client
+        /// </summary>
+        public int Identity { get; set; }
+    }
+}

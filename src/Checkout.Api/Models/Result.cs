f49e6b16270a32e22d9fe6d5935ca2be21bb024d
Will Blackburn <harkole@gmail.com>
2022-05-08 19:45:24 +0100

    Added Result object for handling success/failure results in the controller without bleeding an exception up the chain

---
+++ src/Checkout.Api/Models/Result.cs
@@ -0,0 +1,25 @@
+﻿namespace Checkout.Api.Models
+{
+    /// <summary>
+    /// Provides a result object containing
+    /// either the expected reulst or an error
+    /// </summary>
+    /// <typeparam name="T">The object type that should be returned</typeparam>
+    public class Result<T> where T : class
+    {
+        /// <summary>
+        /// Flag to show if the request was succesful or failure
+        /// </summary>
+        public bool IsSuccess { get; set; }
+
+        /// <summary>
+        /// The object value if present
+        /// </summary>
+        public T? Value { get; set; }
+
+        /// <summary>
+        /// Addition message, expected to be an friendly/safe error message
+        /// </summary>
+        public string? Message { get; set; }
+    }
+}

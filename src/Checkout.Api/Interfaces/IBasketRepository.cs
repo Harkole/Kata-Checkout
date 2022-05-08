966ae629906447243979c99898b25a6fa6cb03b9
Will Blackburn <harkole@gmail.com>
2022-05-08 20:23:30 +0100

    Created simple repsoitory layer for a basket, no assumption of the storage type is made at this time

---
+++ src/Checkout.Api/Interfaces/IBasketRepository.cs
@@ -0,0 +1,37 @@
+﻿using Checkout.Api.Models;
+
+namespace Checkout.Api.Interfaces
+{
+    /// <summary>
+    /// All Basket repository oprerations
+    /// </summary>
+    public interface IBasketRepository
+    {
+        /// <summary>
+        /// Updates the repository with the provided basket details (Create/Update)
+        /// </summary>
+        /// <param name="clientId">Client identity</param>
+        /// <param name="basket">The basket value to store</param>
+        /// <param name="cancellationToken">System cancellation object</param>
+        /// <returns>The Created/Updated basket object</returns>
+        Task<Result<Basket>> StoreBasketAsync(int clientId, Basket basket, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// The basket to 
+        /// </summary>
+        /// <param name="clientId">The client identity</param>
+        /// <param name="basketId">The basket identity</param>
+        /// <param name="cancellationToken">System cancellation object</param>
+        /// <returns>The client basket that was stored</returns>
+        Task<Result<Basket>> GetBasketAsync(int clientId, int basketId, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Removes the stored basket value
+        /// </summary>
+        /// <param name="clientId">The client identity</param>
+        /// <param name="basketId">The basket identity to delete</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>A success/failure flag for deleting the basket</returns>
+        Task<Result<Basket>> DeleteBasketAsync(int clientId, int basketId, CancellationToken cancellationToken);
+    }
+}
